5be50565bf8b49bc8c51b22c685acdd6b4cc240d
William Liao <r05246012@ntu.edu.tw>
2021-06-18 01:45:47 +0800

    major update

--- hal/models/nl_kernel.cu
+++ hal/models/nl_kernel.cu
@@ -143,7 +143,101 @@ std::vector<torch::Tensor> nl_maxpooling_cuda(
     return {output};
 }
 
-// max pooling test kernel
+// max pooling with padding test kernel
+template <typename scalar_t>
+__global__ void nl_maxpooling_withpadding_kernel(
+    const torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> input,
+    torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> input_pad,
+    const int pw, const int ph, const int stride_x, const int stride_y,
+    torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> output) {
+  //batch index
+  int n = blockIdx.x;
+  //channel index
+  int c = threadIdx.x;
+  //height index
+  int w = threadIdx.y;
+  //width index
+  int h = threadIdx.z;
+  //iteration counter
+  int ii, jj;
+  //padding
+  if (n < input_pad.size(0) && c < input_pad.size(1) && w < input_pad.size(2)-1 && w > 0 &&
+      h < input_pad.size(3)-1 && h > 0){
+        input_pad[n][c][w][h] = input[n][c][w-1][h-1];
+  }
+  __syncthreads();
+  //max pooling
+  if (n < output.size(0) && c < output.size(1) && w < output.size(2) && h < output.size(3)){
+    //initialize pooling
+    output[n][c][w][h] = input_pad[n][c][w*stride_x][h*stride_y];
+    for( ii = w*stride_x; ii < w*stride_x+pw; ii++){
+      for( jj = h*stride_y; jj < h*stride_y+ph; jj++){
+        if(input_pad[n][c][ii][jj] > output[n][c][w][h]){
+          output[n][c][w][h] = input_pad[n][c][ii][jj];
+        }
+      }
+    }
+  }
+}
+
+// max pooling with padding test kernel wrapper function
+
+std::vector<torch::Tensor> nl_maxpooling_withpadding_cuda(
+  torch::Tensor input,
+  torch::Tensor poolsize,
+  torch::Tensor stride,
+  torch::Tensor padding){
+    auto input_size = input.sizes();
+    const int num_batch   = input_size[0];
+    const int num_channel = input_size[1];
+    const int width = input_size[2];
+    const int height = input_size[3];
+    auto poolsize_a = poolsize.accessor<float,1>();
+    auto stride_a = stride.accessor<float,1>();
+    auto padding_a = padding.accessor<float,1>();
+
+    std::cout << "Pool window size: (" << poolsize_a[0] << ", " << poolsize_a[1] << ")" << std::endl;
+    std::cout << "Stride size: (" << stride_a[0] << ", " << stride_a[1] << ")" << std::endl;
+    std::cout << "Padding size: (" << padding_a[0] << ", " << padding_a[1] << ")" << std::endl;
+
+    //std::cout << "(N,C,H,W) = (" << num_batch << ", " << num_channel << ", "
+    //<< height << ", " << width << ")" << std::endl;
+
+    //calculate output size
+    const int out_w = floor((width+2*padding_a[0]-poolsize_a[0])/stride_a[0])+1;
+    const int out_h = floor((height+2*padding_a[1]-poolsize_a[1])/stride_a[1])+1;
+    const int padding_w = width+2*padding_a[0];
+    const int padding_h = height+2*padding_a[1];
+    std::cout << "(out_h,out_w) = (" << out_h << ", " << out_w << ")" << std::endl;
+    //ininitalize output
+    torch::Tensor output = torch::zeros({num_batch, num_channel, out_w, out_h},
+      torch::TensorOptions().device(torch::kCUDA));
+    torch::Tensor input_pad = torch::zeros({num_batch, num_channel, padding_w,
+      padding_h}, torch::TensorOptions().device(torch::kCUDA));
+    std::cout << "Output size: " << output.sizes() << std::endl;
+    std::cout << "Input with padding size: " << input_pad.sizes() << std::endl;
+
+    const int threadnum_x = min(padding_w,1024);
+    std::cout << "threadnum x: " << threadnum_x << std::endl;
+    const int threadnum_y = min(padding_h,1024);
+    std::cout << "threadnum y: " << threadnum_y << std::endl;
+    const dim3 block(num_channel,threadnum_x,threadnum_y);
+    std::cout << "block.x: " << block.x << std::endl;
+    std::cout << "block.y: " << block.y << std::endl;
+    std::cout << "block.z: " << block.z << std::endl;
+
+    AT_DISPATCH_FLOATING_TYPES(input.type(), "nl_maxpool_padding_gpu", ([&] {
+      nl_maxpooling_withpadding_kernel<scalar_t><<<num_batch, block>>>(
+          input.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          input_pad.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          poolsize_a[0], poolsize_a[1], stride_a[0], stride_a[1],
+          output.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>());
+    }));
+
+    return {output};
+}
+
+// average pooling test kernel
 template <typename scalar_t>
 __global__ void nl_avgpooling_kernel(
     const torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> input,
@@ -209,7 +303,7 @@ std::vector<torch::Tensor> nl_avgpooling_cuda(
     std::cout << "block.y: " << block.y << std::endl;
     std::cout << "block.z: " << block.z << std::endl;
 
-    AT_DISPATCH_FLOATING_TYPES(input.type(), "nl_maxpool_gpu", ([&] {
+    AT_DISPATCH_FLOATING_TYPES(input.type(), "nl_avgpool_gpu", ([&] {
       nl_avgpooling_kernel<scalar_t><<<num_batch, block>>>(
           input.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
           poolsize_a[0], poolsize_a[1], stride_a[0], stride_a[1],
@@ -219,14 +313,218 @@ std::vector<torch::Tensor> nl_avgpooling_cuda(
     return {output};
 }
 
+// average pooling with padding test kernel
+template <typename scalar_t>
+__global__ void nl_avgpooling_withpadding_kernel(
+    const torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> input,
+    torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> input_pad,
+    const int pw, const int ph, const int stride_x, const int stride_y,
+    torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> output) {
+  //batch index
+  int n = blockIdx.x;
+  //channel index
+  int c = threadIdx.x;
+  //height index
+  int w = threadIdx.y;
+  //width index
+  int h = threadIdx.z;
+  //iteration counter
+  int ii, jj;
+  //padding
+  if (n < input_pad.size(0) && c < input_pad.size(1) && w < input_pad.size(2)-1 && w > 0 &&
+      h < input_pad.size(3)-1 && h > 0){
+        input_pad[n][c][w][h] = input[n][c][w-1][h-1];
+  }
+  __syncthreads();
+  //average pooling
+  if (n < output.size(0) && c < output.size(1) && w < output.size(2) && h < output.size(3)){
+    //initialize pooling
+    output[n][c][w][h] = 0.0;
+    for( ii = w*stride_x; ii < w*stride_x+pw; ii++){
+      for( jj = h*stride_y; jj < h*stride_y+ph; jj++){
+        output[n][c][w][h] += input_pad[n][c][ii][jj];
+      }
+    }
+    output[n][c][w][h] = output[n][c][w][h]/(pw*ph);
+  }
+}
+
+// average pooling with padding test kernel wrapper function
+
+std::vector<torch::Tensor> nl_avgpooling_withpadding_cuda(
+  torch::Tensor input,
+  torch::Tensor poolsize,
+  torch::Tensor stride,
+  torch::Tensor padding){
+    auto input_size = input.sizes();
+    const int num_batch   = input_size[0];
+    const int num_channel = input_size[1];
+    const int width = input_size[2];
+    const int height = input_size[3];
+    auto poolsize_a = poolsize.accessor<float,1>();
+    auto stride_a = stride.accessor<float,1>();
+    auto padding_a = padding.accessor<float,1>();
+
+    std::cout << "Pool window size: (" << poolsize_a[0] << ", " << poolsize_a[1] << ")" << std::endl;
+    std::cout << "Stride size: (" << stride_a[0] << ", " << stride_a[1] << ")" << std::endl;
+    std::cout << "Padding size: (" << padding_a[0] << ", " << padding_a[1] << ")" << std::endl;
+
+    //std::cout << "(N,C,H,W) = (" << num_batch << ", " << num_channel << ", "
+    //<< height << ", " << width << ")" << std::endl;
+
+    //calculate output size
+    const int out_w = floor((width+2*padding_a[0]-poolsize_a[0])/stride_a[0])+1;
+    const int out_h = floor((height+2*padding_a[1]-poolsize_a[1])/stride_a[1])+1;
+    const int padding_w = width+2*padding_a[0];
+    const int padding_h = height+2*padding_a[1];
+    std::cout << "(out_h,out_w) = (" << out_h << ", " << out_w << ")" << std::endl;
+    //ininitalize output
+    torch::Tensor output = torch::zeros({num_batch, num_channel, out_w, out_h},
+      torch::TensorOptions().device(torch::kCUDA));
+    torch::Tensor input_pad = torch::zeros({num_batch, num_channel, padding_w,
+      padding_h}, torch::TensorOptions().device(torch::kCUDA));
+    std::cout << "Output size: " << output.sizes() << std::endl;
+    std::cout << "Input with padding size: " << input_pad.sizes() << std::endl;
+
+    const int threadnum_x = min(padding_w,1024);
+    std::cout << "threadnum x: " << threadnum_x << std::endl;
+    const int threadnum_y = min(padding_h,1024);
+    std::cout << "threadnum y: " << threadnum_y << std::endl;
+    const dim3 block(num_channel,threadnum_x,threadnum_y);
+    std::cout << "block.x: " << block.x << std::endl;
+    std::cout << "block.y: " << block.y << std::endl;
+    std::cout << "block.z: " << block.z << std::endl;
+
+    AT_DISPATCH_FLOATING_TYPES(input.type(), "nl_avgpool_padding_gpu", ([&] {
+      nl_avgpooling_withpadding_kernel<scalar_t><<<num_batch, block>>>(
+          input.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          input_pad.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          poolsize_a[0], poolsize_a[1], stride_a[0], stride_a[1],
+          output.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>());
+    }));
+
+    return {output};
+}
+
 // forward kernel function
+template <typename scalar_t>
+__global__ void nl_forward_kernel(
+    const torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> input,
+    const torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> conv_input,
+    torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> input_pad,
+    const int pw, const int ph, const int stride_x, const int stride_y,
+    torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> output1,
+    torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> output2,
+    torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> output3) {
+  //batch index
+  int n = blockIdx.x;
+  //channel index
+  int c = threadIdx.x;
+  //height index
+  int w = threadIdx.y;
+  //width index
+  int h = threadIdx.z;
+  //iteration counter
+  int ii, jj;
+  // relu(conv(x))
+  if (n < conv_input.size(0) && c < conv_input.size(1) && w < conv_input.size(2) && 
+      h < conv_input.size(3)){
+    output1[n][c][w][h] = relu(conv_input[n][c][w][h]);
+  }
+  //padding
+  if (n < input_pad.size(0) && c < input_pad.size(1) && w < input_pad.size(2)-1 && w > 0 &&
+      h < input_pad.size(3)-1 && h > 0){
+        input_pad[n][c][w][h] = input[n][c][w-1][h-1];
+  }
+  __syncthreads();
+  //max pooling
+  if (n < output2.size(0) && c < output2.size(1) && w < output2.size(2) && h < output2.size(3)){
+    //initialize pooling
+    output2[n][c][w][h] = input_pad[n][c][w*stride_x][h*stride_y];
+    for( ii = w*stride_x; ii < w*stride_x+pw; ii++){
+      for( jj = h*stride_y; jj < h*stride_y+ph; jj++){
+        if(input_pad[n][c][ii][jj] > output2[n][c][w][h]){
+          output2[n][c][w][h] = input_pad[n][c][ii][jj];
+        }
+      }
+    }
+  }
+  //average pooling
+  if (n < output3.size(0) && c < output3.size(1) && w < output3.size(2) && h < output3.size(3)){
+    //initialize pooling
+    output3[n][c][w][h] = 0.0;
+    for( ii = w*stride_x; ii < w*stride_x+pw; ii++){
+      for( jj = h*stride_y; jj < h*stride_y+ph; jj++){
+        output3[n][c][w][h] += input_pad[n][c][ii][jj];
+      }
+    }
+    output3[n][c][w][h] = output3[n][c][w][h]/(pw*ph);
+  }
+}
 
 // forward kernel wrapper function
-/*
-std::vector<torch::Tensor> lltm_cuda_forward(
+
+std::vector<torch::Tensor> nl_forward_cuda(
   torch::Tensor input,
-  torch::Tensor weights,
-  torch::Tensor bias,
-  torch::Tensor old_h,
-  torch::Tensor old_cell){}
-*/
+  torch::Tensor conv_input,
+  torch::Tensor poolsize,
+  torch::Tensor stride,
+  torch::Tensor padding){
+    auto input_size = input.sizes();
+    const int num_batch   = input_size[0];
+    const int num_channel = input_size[1];
+    const int width = input_size[2];
+    const int height = input_size[3];
+    auto poolsize_a = poolsize.accessor<float,1>();
+    auto stride_a = stride.accessor<float,1>();
+    auto padding_a = padding.accessor<float,1>();
+
+    std::cout << "Pool window size: (" << poolsize_a[0] << ", " << poolsize_a[1] << ")" << std::endl;
+    std::cout << "Stride size: (" << stride_a[0] << ", " << stride_a[1] << ")" << std::endl;
+    std::cout << "Padding size: (" << padding_a[0] << ", " << padding_a[1] << ")" << std::endl;
+
+    //std::cout << "(N,C,H,W) = (" << num_batch << ", " << num_channel << ", "
+    //<< height << ", " << width << ")" << std::endl;
+
+    //calculate output size
+    const int out_w = floor((width+2*padding_a[0]-poolsize_a[0])/stride_a[0])+1;
+    const int out_h = floor((height+2*padding_a[1]-poolsize_a[1])/stride_a[1])+1;
+    const int padding_w = width+2*padding_a[0];
+    const int padding_h = height+2*padding_a[1];
+    std::cout << "(out_h,out_w) = (" << out_h << ", " << out_w << ")" << std::endl;
+    //ininitalize output
+    torch::Tensor output1 = torch::zeros({num_batch, num_channel, out_w, out_h},
+      torch::TensorOptions().device(torch::kCUDA));
+    torch::Tensor output2 = torch::zeros({num_batch, num_channel, out_w, out_h},
+      torch::TensorOptions().device(torch::kCUDA));
+    torch::Tensor output3 = torch::zeros({num_batch, num_channel, out_w, out_h},
+      torch::TensorOptions().device(torch::kCUDA));
+    torch::Tensor input_pad = torch::zeros({num_batch, num_channel, padding_w,
+      padding_h}, torch::TensorOptions().device(torch::kCUDA));
+    std::cout << "Output1 size: " << output1.sizes() << std::endl;
+    std::cout << "Output2 size: " << output2.sizes() << std::endl;
+    std::cout << "Output3 size: " << output3.sizes() << std::endl;
+    std::cout << "Input with padding size: " << input_pad.sizes() << std::endl;
+
+    const int threadnum_x = min(padding_w,1024);
+    std::cout << "threadnum x: " << threadnum_x << std::endl;
+    const int threadnum_y = min(padding_h,1024);
+    std::cout << "threadnum y: " << threadnum_y << std::endl;
+    const dim3 block(num_channel,threadnum_x,threadnum_y);
+    std::cout << "block.x: " << block.x << std::endl;
+    std::cout << "block.y: " << block.y << std::endl;
+    std::cout << "block.z: " << block.z << std::endl;
+
+    AT_DISPATCH_FLOATING_TYPES(input.type(), "nl_forward_gpu", ([&] {
+      nl_forward_kernel<scalar_t><<<num_batch, block>>>(
+          input.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          conv_input.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          input_pad.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          poolsize_a[0], poolsize_a[1], stride_a[0], stride_a[1],
+          output1.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          output2.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>(),
+          output3.packed_accessor32<scalar_t,4,torch::RestrictPtrTraits>());
+    }));
+
+    return {output1, output2, output3};
+  }
--- hal/models/nl_wrapper.cpp
+++ hal/models/nl_wrapper.cpp
@@ -86,7 +86,48 @@ std::vector<torch::Tensor> nl_maxpooling(torch::Tensor input, torch::Tensor pool
     return nl_maxpooling_cuda(input, poolsize, stride);
 }
 
-// max pooling wrapper
+// max pooling with padding wrapper
+std::vector<torch::Tensor> nl_maxpooling_withpadding_cuda(
+    torch::Tensor input,
+    torch::Tensor poolsize,
+    torch::Tensor stride,
+    torch::Tensor padding
+);
+
+std::vector<torch::Tensor> nl_maxpooling_withpadding(torch::Tensor input, torch::Tensor poolsize,
+  torch::Tensor stride, torch::Tensor padding){
+    auto input_size = input.sizes();
+    auto pool_size = poolsize.sizes();
+    auto stride_size = stride.sizes();
+    auto padding_size = padding.sizes();
+    //auto poolsize_a = poolsize.accessor<float,1>();
+    //auto stride_a = stride.accessor<float,1>();
+    
+    if ( input_size.size() != 4)
+    {
+        std::cout << "Error! Input must be a 4D tensor." << std::endl;
+        abort();
+    }
+    if ( pool_size[0] != 2){
+        std::cout << "Error! Pool size must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    if ( stride_size[0] != 2){
+        std::cout << "Error! Stride must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    if ( padding_size[0] != 2){
+        std::cout << "Error! Stride must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    CHECK_INPUT(input);
+    //CHECK_INPUT(poolsize);
+    //CHECK_INPUT(stride);
+
+    return nl_maxpooling_withpadding_cuda(input, poolsize, stride, padding);
+}
+
+// average pooling wrapper
 std::vector<torch::Tensor> nl_avgpooling_cuda(
     torch::Tensor input,
     torch::Tensor poolsize,
@@ -121,36 +162,101 @@ std::vector<torch::Tensor> nl_avgpooling(torch::Tensor input, torch::Tensor pool
     return nl_avgpooling_cuda(input, poolsize, stride);
 }
 
+// average pooling with padding wrapper
+std::vector<torch::Tensor> nl_avgpooling_withpadding_cuda(
+    torch::Tensor input,
+    torch::Tensor poolsize,
+    torch::Tensor stride,
+    torch::Tensor padding
+);
+
+std::vector<torch::Tensor> nl_avgpooling_withpadding(torch::Tensor input, torch::Tensor poolsize,
+  torch::Tensor stride, torch::Tensor padding){
+    auto input_size = input.sizes();
+    auto pool_size = poolsize.sizes();
+    auto stride_size = stride.sizes();
+    auto padding_size = padding.sizes();
+    //auto poolsize_a = poolsize.accessor<float,1>();
+    //auto stride_a = stride.accessor<float,1>();
+    
+    if ( input_size.size() != 4)
+    {
+        std::cout << "Error! Input must be a 4D tensor." << std::endl;
+        abort();
+    }
+    if ( pool_size[0] != 2){
+        std::cout << "Error! Pool size must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    if ( stride_size[0] != 2){
+        std::cout << "Error! Stride must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    if ( padding_size[0] != 2){
+        std::cout << "Error! Stride must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    CHECK_INPUT(input);
+    //CHECK_INPUT(poolsize);
+    //CHECK_INPUT(stride);
+
+    return nl_avgpooling_withpadding_cuda(input, poolsize, stride, padding);
+}
+
 // CUDA forward declarations
 
-/*
-std::vector<torch::Tensor> lltm_cuda_forward(
-    torch::Tensor input,
-    torch::Tensor weights,
-    torch::Tensor bias,
-    torch::Tensor old_h,
-    torch::Tensor old_cell);
 
-std::vector<torch::Tensor> lltm_forward(
+std::vector<torch::Tensor> nl_forward_cuda(
     torch::Tensor input,
-    torch::Tensor weights,
-    torch::Tensor bias,
-    torch::Tensor old_h,
-    torch::Tensor old_cell) {
-  CHECK_INPUT(input);
-  CHECK_INPUT(weights);
-  CHECK_INPUT(bias);
-  CHECK_INPUT(old_h);
-  CHECK_INPUT(old_cell);
+    torch::Tensor conv_input,
+    torch::Tensor poolsize,
+    torch::Tensor stride,
+    torch::Tensor padding);
 
-  return lltm_cuda_forward(input, weights, bias, old_h, old_cell);
+std::vector<torch::Tensor> nl_forward(
+    torch::Tensor input,
+    torch::Tensor conv_input,
+    torch::Tensor poolsize,
+    torch::Tensor stride,
+    torch::Tensor padding) {
+    auto input_size = input.sizes();
+    auto pool_size = poolsize.sizes();
+    auto stride_size = stride.sizes();
+    auto padding_size = padding.sizes();
+    //auto poolsize_a = poolsize.accessor<float,1>();
+    //auto stride_a = stride.accessor<float,1>();
+    
+    if ( input_size.size() != 4)
+    {
+        std::cout << "Error! Input must be a 4D tensor." << std::endl;
+        abort();
+    }
+    if ( pool_size[0] != 2){
+        std::cout << "Error! Pool size must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    if ( stride_size[0] != 2){
+        std::cout << "Error! Stride must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    if ( padding_size[0] != 2){
+        std::cout << "Error! Stride must be a 1D tensor with 2 elements." << std::endl;
+        abort();
+    }
+    CHECK_INPUT(input);
+    //CHECK_INPUT(poolsize);
+    //CHECK_INPUT(stride);
+
+    return nl_forward_cuda(input, conv_input, poolsize, stride, padding);
 }
-*/
+
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  //m.def("forward", &lltm_forward, "LLTM forward (CUDA)");
+  m.def("nl_forward", &nl_forward, "nl forward (CUDA)");
   m.def("getTensorSize", &getTensorSize, "Get tensor size");
   m.def("nl_relu", &nl_relu, "compute ReLU");
   m.def("nl_maxpooling", &nl_maxpooling, "compute max pooling");
+  m.def("nl_maxpooling_withpadding", &nl_maxpooling_withpadding, "compute max pooling with padding");
   m.def("nl_avgpooling", &nl_avgpooling, "compute average pooling");
+  m.def("nl_avgpooling_withpadding", &nl_avgpooling_withpadding, "compute average pooling with padding");
 }
